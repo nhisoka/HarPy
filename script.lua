fe172b3d4ce8d45338975767270c66fd4a71332f
xShadow1234 <135708281+xShadow1234@users.noreply.github.com>
2023-07-07 11:42:56 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -2,4 +2,6 @@
         loadstring(game:HttpGet("https://raw.githubusercontent.com/xShadow1234/HarPy/main/Arm%20Wrestle%20Simulator.lua"))()
     elseif game.PlaceId == 6938803436 or game.PlaceId == 6990129309 or game.PlaceId == 6990133340 or game.PlaceId == 6990131029 or game.PlaceId == 7338881230 or game.PlaceId == 7274690025 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/xShadow1234/HarPy/main/AnimeDimensions.lua"))()
+    elseif game.PlaceId == 155615604 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/xShadow1234/HarPy/main/Prison%20Life.lua"))()
     end
