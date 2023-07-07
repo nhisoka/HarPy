34b5f2b717b6e5faa1f29f8de0f195c75db482c6
xShadow1234 <135708281+xShadow1234@users.noreply.github.com>
2023-07-07 08:59:35 +0200

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,5 @@
+    if game.PlaceId == 13127800756 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/xShadow1234/HarPy/main/Arm%20Wrestle%20Simulator.lua"))()
+    elseif game.PlaceId == 6938803436 or game.PlaceId == 6990129309 or game.PlaceId == 6990133340 or game.PlaceId == 6990131029 or game.PlaceId == 7338881230 or game.PlaceId == 7274690025 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/xShadow1234/HarPy/main/Anime%20Dimensions.lua"))()
+    end
