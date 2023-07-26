d1e41d4d089c67a4392cf2db58ad27a784bfdb15
xShadow1234 <135708281+xShadow1234@users.noreply.github.com>
2023-07-26 15:30:30 +0200

    Create HarPy_loader.lua

---
+++ HarPy_loader.lua
@@ -0,0 +1,9 @@
+if game.PlaceId == 13127800756 then
+    loadstring(game:HttpGet('https://scripts.luawl.com/hosted/5621/20513/HarPy.lua'))()
+elseif game.PlaceId == 11445923563 then
+    loadstring(game:HttpGet('https://scripts.luawl.com/hosted/5621/20670/OneFruitsim.lua'))()
+elseif game.PlaceId == 6938803436 then
+    loadstring(game:HttpGet('https://scripts.luawl.com/hosted/5621/20671/AnimeDimensions.lua'))()
+elseif game.PlaceId == 8425128940 then
+    loadstring(game:HttpGet('https://scripts.luawl.com/hosted/5621/20672/AnimeWarriors2.lua'))()
+end
