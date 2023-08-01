12e1adac4d15638be7079306e31fd5b8dff662d7
xShadow1234 <135708281+xShadow1234@users.noreply.github.com>
2023-08-01 18:25:42 +0200

    Update HarPy_loader.lua

--- HarPy_loader.lua
+++ HarPy_loader.lua
@@ -1,6 +1,6 @@
 if game.PlaceId == 13127800756 then
     loadstring(game:HttpGet('https://scripts.luawl.com/hosted/5621/20513/HarPy.lua'))()
-elseif game.PlaceId == 11445923563 then
+elseif game.PlaceId == 11445923563 or game.PlaceId == 13155198714 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/xShadow1234/HarPy/main/One%20Fruit.lua'))()
 elseif game.PlaceId == 6938803436 then
     loadstring(game:HttpGet('https://scripts.luawl.com/hosted/5621/20671/AnimeDimensions.lua'))()
