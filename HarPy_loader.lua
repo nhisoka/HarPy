015b024090406c4770c519e3d6a68da726b959b5
xShadow1234 <135708281+xShadow1234@users.noreply.github.com>
2023-08-27 10:45:22 +0200

    Update HarPy_loader.lua

--- HarPy_loader.lua
+++ HarPy_loader.lua
@@ -16,4 +16,6 @@ elseif game.PlaceId == 13028445510 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/xShadow1234/HarPy/main/Shoot%20Wall%20Simulator.lua'))()
 elseif game.PlaceId == 6299805723 then
     loadstring(game:HttpGet('https://raw.githubusercontent.com/xShadow1234/HarPy/main/Anime%20Fighters%20Dupe.lua'))()
+elseif game.PlaceId == 11860394049 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Killa5676/Killa-s-Scripts/main/lumber%20legends"))()
 end
